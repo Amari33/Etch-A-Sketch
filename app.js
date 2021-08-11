eef1efab3cd199f80e1a4a688618701aabe41bf2
Amari Adams <adamsamari347@gmail.com>
2021-08-10 21:04:07 -0400

    i

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ let n=16, i=-1, j=0, s='';
 
 while(++i<n) {
   s+= '<div class="row">'
-  for(j=0; j<n; j++) s+= `<div id="cell" class="cell"> ${(i*n+j).toString(16)} </div>`;
+  for(j=0; j<n; j++) s+= 
+  `<div id="cell" class="cell"> ${(i*n+j).toString(16)} </div>`;
   s+= '</div>'
 }
 
